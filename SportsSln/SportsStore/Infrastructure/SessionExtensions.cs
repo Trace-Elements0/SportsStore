b010751dc2f4c995f4709f9de8833df0bb36fc24
Trace-Elements0 <62453774+Trace-Elements0@users.noreply.github.com>
2020-09-16 20:25:25 -0400

    sports sln added

---
+++ SportsSln/SportsStore/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+namespace SportsStore.Infrastructure
+{
+    public static class SessionExtensions
+    {//Class file provides access to the session state data to serialize Cart objects into JSON and convert them back
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+            return sessionData == null
+                ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
+        }
+    }
+}
