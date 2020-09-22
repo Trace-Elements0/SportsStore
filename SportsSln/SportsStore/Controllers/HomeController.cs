a8fd9bd83663b0b81ba1b6718d7da45f49d1e779
Jorge Sanchez <jorge.a.sanchezII@outlook.com>
2020-09-22 09:01:51 -0400

    Changes

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -2,14 +2,11 @@
 using SportsStore.Models;
 using System.Linq;
 using SportsStore.Models.ViewModels;
-
-
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
-
         public int PageSize = 4;
         public HomeController(IStoreRepository repo)
         {
